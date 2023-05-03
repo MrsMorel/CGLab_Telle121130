04944f52f2c28fd6125e38d2ccbee7d8cd1f0e92
Xinyu Zhang <104454190+XinyuWZHANG@users.noreply.github.com>
2023-05-03 22:43:48 +0200

    Create solar_System.hpp

---
+++ application/include/solar_System.hpp
@@ -0,0 +1,14 @@
+#ifndef SOLAR_SYSTEM_HPP
+#define SOLAR_SYSTEM_HPP
+#include "application_solar.hpp"
+
+class solar_System : public ApplicationSolar {
+  public:
+    //create sun and other planets, including name, position and size
+    void init(); 
+    //update is based on the orientation and position
+    void update();
+    //display sun and other planets
+    void render();
+};
+#ENDIF
