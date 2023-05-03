946b3ae83fa57f7fc92bcd7b2f98ec38b777a153
Xinyu Zhang <104454190+XinyuWZHANG@users.noreply.github.com>
2023-05-03 20:35:35 +0200

    Update PointLightNode.h

--- framework/include/PointLightNode.h
+++ framework/include/PointLightNode.h
@@ -13,16 +13,21 @@ public:
     PointLightNode();
     PointLightNode(const glm::vec3& lightColor);
     PointLightNode(const glm::vec3& lightColor, float lightIntensity); 
-    //Do we need to add glm::vec3 position
+    //PointLightNode(const glm::vec3& position, const glm::vec3& lightColor, float lightIntensity);
     ~PointLightNode() override; //destructor
+    
     //getter and setter
     float getLightIntensity() const;
     void setLightIntensity(float new_LightIntensity);
     glm::vec3 getLightColor() const;
     void setLightColor(const glm::vec3& new_LightColor);
+    //void setPosition(const glm::vec3& position);
+    //glm::vec3 getPosition() const;
+    
 private:
     float lightIntensity_{};
     glm::vec3 lightColor_;
+    //glm::vec3 
 };
 
 #endif //OPENGL_FRAMEWORK_POINTLIGHTNODE_H
